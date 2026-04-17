d0acb1d1e4c3543c4d3a64e97a6d2093a14292eb
kmaciaze <maciazek.kasia@gmail.com>
2026-04-18 00:50:56 +0200

    Update  Bazy danych i manipulacja danymi - Zadanie 5.sql

--- Bazy danych i manipulacja danymi - Zadanie 5.sql
+++ Bazy danych i manipulacja danymi - Zadanie 5.sql
@@ -10,7 +10,7 @@ CREATE TABLE Zainteresowania (
     Data_realizacji DATE
 );
 
--- 3. Wprowadzenie danych tylko dla Nazwa i Opis
+-- 3. Wprowadzenie danych tylko dla nazwa i opis
 INSERT INTO Zainteresowania (ID, Nazwa, Opis) VALUES
 (1, 'Nauka języka koreańskiego', '15 minut dziennie'),
 (2, 'Basen', 'Basen 2 razy w tygodniu'),
@@ -25,7 +25,7 @@ SELECT * FROM Zainteresowania;
 INSERT INTO Zainteresowania (ID, Nazwa, Opis, Data_realizacji)
 VALUES (6, 'Kino','Film The drama', '2026-04-10');
 
--- 6. Dodanie brakujących danych dla wiersza 4 (UPDATE)
+-- 6. Dodanie brakujących danych dla wiersza 4
 UPDATE Zainteresowania 
 SET Data_realizacji = '2026-03-24' 
 WHERE ID = 4;
@@ -33,7 +33,7 @@ WHERE ID = 4;
 -- 7. Sprawdzenie wszystkich pozycji
 SELECT * FROM Zainteresowania;
 
--- 8. Usuń wiersze, w których jakaś pozycja jest pusta
+-- 8. Usuniecie wierszy, w których jakaś pozycja jest pusta
 DELETE FROM Zainteresowania 
 WHERE Data_realizacji IS NULL;
 
